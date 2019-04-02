7cb5b9dac5d664ad66831968f0de3471899de66d
Mishkatchowdhury <47510454+Mishkatchowdhury@users.noreply.github.com>
2019-04-02 23:21:19 +0600

    Add files via upload

---
+++ bankMenagement/src/bankmenagement/BankMenagement.java
@@ -0,0 +1,12 @@
+
+package bankmenagement;
+
+public class BankMenagement {
+
+    public static void main(String[] args) {
+     Introduction obj=new Introduction();
+       obj.setVisible(true);
+    } 
+    
+}
+    
